2a3128ac95c983cfcfc95a9d7e789dcdbdb20f3b
IzabelaLas <izabela.laszczuk@gmail.com>
2022-06-30 12:36:03 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import WeatherSearch from "./WeatherSearch";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-      <br />
-      <a href="https://github.com/IzabelaLas/weather-react">
-        Open source-code
-      </a>{" "}
-      by Izabela Laszczuk
+      <div className="container">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+        <br />
+        <a href="https://github.com/IzabelaLas/weather-react">
+          Open source-code
+        </a>{" "}
+        by Izabela Laszczuk
+      </div>
     </div>
   );
 }
